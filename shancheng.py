3627d07f57bccf1ce01a4c59ca7df03c35bdb357
chenhongju <864196621@qq.com>
2019-03-02 16:22:01 +0800

    update

--- shancheng.py
+++ shancheng.py
@@ -132,13 +132,13 @@ def send_email(msg):
     # 163邮箱服务器地址
     mail_host = 'smtp.163.com'
     # 163用户名
-    mail_user = '15082408104'
+    mail_user = '150xxxx8104'
     # 密码(部分邮箱为授权码)
-    mail_pass = 'laoju123456'
+    mail_pass = 'xxxx'
     # 邮件发送方邮箱地址
-    sender = '15082408104@163.com'
+    sender = '150xxxx8104@163.com'
     # 邮件接受方邮箱地址，注意需要[]包裹，这意味着你可以写多个邮件地址群发
-    receivers = ['864196621@qq.com']
+    receivers = ['xxxxxx@qq.com']
 
     # 设置email信息
     # 邮件内容设置
